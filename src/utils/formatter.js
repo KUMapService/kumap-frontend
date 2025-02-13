b4ed6dfb0cf8c6ed41baf87af203b2ccf0737af1
ehghks021203 <ehghks021203@gmail.com>
2025-02-13 21:54:12 +0900

    refactor: extract NumberFormat function for reuse

---
+++ src/utils/formatter.js
@@ -0,0 +1,18 @@
+export const NumberFormat = (number) => {
+  // 조 단위 포멧팅
+  if (Math.floor(number / 1000000000000) !== 0) {
+    return Math.floor(number / 1000000000000).toLocaleString('ko-KR') + '조';
+  }
+  // 억 단위 포멧팅
+  else if (Math.floor(number / 100000000) !== 0) {
+    return Math.floor(number / 100000000).toLocaleString('ko-KR') + '억';
+  }
+  // 만 단위 포멧팅
+  else if (Math.floor(number / 10000) !== 0) {
+    return Math.floor(number / 10000).toLocaleString('ko-KR') + '만';
+  }
+  // 그 외
+  else {
+    return Math.floor(number).toLocaleString('ko-KR');
+  }
+};
