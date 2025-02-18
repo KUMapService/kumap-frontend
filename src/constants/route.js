ae9515b98e45a6a43af51a1bd604c17acb3b0e81
ehghks021203 <ehghks021203@gmail.com>
2025-02-19 02:28:04 +0900

    feat: add endpoint route

--- src/constants/route.js
+++ src/constants/route.js
@@ -6,5 +6,11 @@ export const API = {
     REGISTER: '/auth/register',
     RESET_PASSWORD: '/auth/reset-password',
     VERIFY_TOKEN: '/auth/protected',
+    REFRESH_TOKEN: '/auth/refresh-token',
+  },
+  USER: {
+    RESET_PASSWORD: '/user/reset-password',
+    MODIFY_USER_INFO: '/user/modify-user-info',
+    CHANGE_PASSWORD: '/user/change-password',
   },
 };
