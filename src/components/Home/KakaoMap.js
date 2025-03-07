1339d7e3e129e180e650ea034d714f1aca64426f
ehghks021203 <ehghks021203@gmail.com>
2025-03-07 09:08:44 +0900

    feat: add kakaomap

--- src/components/Home/KakaoMap.js
+++ src/components/Home/KakaoMap.js
@@ -2,39 +2,129 @@
 const { kakao } = window;
 
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BiCurrentLocation } from 'react-icons/bi';
 import { MdOutlineMap } from 'react-icons/md';
+import palette from '@constants/styles';
 import { MapContainer, MapButton } from '@styles/Home/KakaoMap/KakaoMap.styles';
 import '@styles/Home/KakaoMap/KakaoMap.css';
+import { getCadastralMap, getPNU } from 'api/geo';
+import { toast } from 'react-toastify';
 
 const KakaoMap = () => {
+  // global variables
+  const dispatch = useDispatch();
+  const currentLandAddress = useSelector((state) => state.land.currentLandAddress);
+  // map variables
   const [map, setMap] = useState(null);
   const [isSkyView, setSkyView] = useState(false);
+  // overlay marker variables
+  const [cadastralMap, setCadastralMap] = useState([]);
+  const [currentLandMarker, setCurrentLandMarker] = useState(null);
 
   useEffect(() => {
     initializeMap();
   }, []);
 
   const initializeMap = () => {
-    if (map) {
-      return;
-    }
     const lastViewLat = localStorage.getItem('last_view_lat');
     const lastViewLng = localStorage.getItem('last_view_lng');
-    // 만약 값이 존재한다면 해당 위경도로 중심 좌표를 변경하고, 아니면 내가 임의로 지정한 좌표로 이동하게 돼.
     const initialCenter =
       lastViewLat && lastViewLng
         ? new kakao.maps.LatLng(lastViewLat, lastViewLng)
         : new kakao.maps.LatLng(37.536172, 126.976978);
 
-    // 카카오맵의 기본 설정을 진행해. 줌 레벨은 3으로 할꺼야.
     const mapOptions = {
       center: initialCenter,
       level: 3,
     };
-    // 새로운 카카오맵을 만들고, html은 id가 map으로 되어있는 요소와 연결해.
     const newMap = new kakao.maps.Map(document.getElementById('map'), mapOptions);
+    const newCustomOverlay = new kakao.maps.CustomOverlay({
+      map: newMap,
+      position: null,
+      content: '',
+      yAnchor: 0,
+    });
     setMap(newMap);
+    setCadastralMap([]);
+    setCurrentLandMarker(newCustomOverlay);
+  };
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+    currentLandMarker.setMap(map);
+    // add event listener
+    kakao.maps.event.addListener(map, 'click', onMapClick);
+  }, [map]);
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+    cadastralMap.forEach((polygon) => {
+      polygon.setMap(null);
+    });
+
+    // set current land marker
+    const latlng = new kakao.maps.LatLng(currentLandAddress?.lat, currentLandAddress?.lng);
+    var content = '';
+    if (!currentLandAddress?.address?.eupmyeondong) {
+      content = `<div class="custom-overlay"><span class="title">불러오는중...<br>`;
+    } else {
+      content = `<div class="custom-overlay"><span class="title">${
+        currentLandAddress?.address?.eupmyeondong + ' ' + currentLandAddress?.address?.detail
+      }<br>`;
+    }
+    currentLandMarker.setContent(content);
+    currentLandMarker.setPosition(latlng);
+    currentLandMarker.setMap(map);
+    map.panTo(latlng);
+
+    // renew cadastral map
+    const fetchCadastralMap = async (pnu) => {
+      try {
+        const response = await getCadastralMap({ pnu: pnu });
+
+        // 새로운 폴리곤 생성
+        const newPolygons = [];
+        for (var i = 0; i < response.polygons.length; i++) {
+          for (var j = 0; j < response.polygons[i].length; j++) {
+            var path = new Array();
+            for (var k = 0; k < response.polygons[i][j].length; k++) {
+              var polygonLatlng = new kakao.maps.LatLng(response.polygons[i][j][k][1], response.polygons[i][j][k][0]);
+              path.push(polygonLatlng);
+            }
+            console.log(path);
+            const polygon = new kakao.maps.Polygon({
+              path,
+              strokeWeight: 2,
+              strokeColor: palette.blue500,
+              strokeOpacity: 0.8,
+              strokeStyle: 'solid',
+              fillColor: palette.blue100,
+              fillOpacity: 0.7,
+            });
+            polygon.setMap(map);
+            newPolygons.push(polygon);
+          }
+        }
+        console.log('New polygons array:', newPolygons);
+        setCadastralMap(newPolygons);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    fetchCadastralMap(currentLandAddress?.pnu);
+  }, [currentLandAddress]);
+
+  const onMapClick = async (mouseEvent) => {
+    if (map.getLevel() > 4) {
+      return;
+    }
+    const latlng = mouseEvent.latLng;
+    await getPNU(dispatch, { lat: latlng.getLat(), lng: latlng.getLng() });
   };
 
   const changeMapType = () => {
